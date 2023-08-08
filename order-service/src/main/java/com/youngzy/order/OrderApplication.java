8d56172a32f7403e8b40701f66e475cf8f754397
young-win <young.zhu.2012@gmail.com>
2023-08-08 18:05:00 +0800

    利用RestTemplate发起HTTP请求，获取用户信息

--- order-service/src/main/java/com/youngzy/order/OrderApplication.java
+++ order-service/src/main/java/com/youngzy/order/OrderApplication.java
@@ -3,13 +3,18 @@ package com.youngzy.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.youngzy.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- order-service/src/main/java/com/youngzy/order/service/OrderService.java
+++ order-service/src/main/java/com/youngzy/order/service/OrderService.java
@@ -2,18 +2,29 @@ package com.youngzy.order.service;
 
 import com.youngzy.order.mapper.OrderMapper;
 import com.youngzy.order.pojo.Order;
+import com.youngzy.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+
+        // 2. 利用RestTemplate发起HTTP请求，获取用户信息
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        // 3. set user
+        order.setUser(user);
+
         // 4.返回
         return order;
     }
