666a41d253597267c6c2ad4ed9b5303a1f7c828c
young-win <young.zhu.2012@gmail.com>
2023-08-14 18:39:19 +0800

    Gateway

---
+++ gateway/src/main/java/com/youngzy/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.youngzy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author youngzy
+ * @since 2023-08-14
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
