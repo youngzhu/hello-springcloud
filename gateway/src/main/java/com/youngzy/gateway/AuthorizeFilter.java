e153ecc3bfdbd481fcb6cd005c592fbb928a570a
young-win <young.zhu.2012@gmail.com>
2023-08-15 09:23:31 +0800

    Gateway - GlobalFilter

---
+++ gateway/src/main/java/com/youngzy/gateway/AuthorizeFilter.java
@@ -0,0 +1,42 @@
+package com.youngzy.gateway;
+
+import lombok.var;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author youngzy
+ * @since 2023-08-14
+ */
+//@Order(-1) // 同实现接口一样的效果
+@Component
+public class AuthorizeFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1. 获取请求参数
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String, String> params = request.getQueryParams();
+        String auth = params.getFirst("authorization");
+        // 2. 判断指定参数
+        if ("admin".equals(auth)) {
+            // 2.1 放行
+            return chain.filter(exchange);
+        }
+        // 2.2 拦截
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
